b6782bcf70a33227894d22c55b022a3ec1fb69c4
Jaromir <jaromircharles@hotmail.com>
2020-05-13 14:18:23 +0200

    retriev data from vessels and save to logbook

--- src/main/java/nz/co/fortytwo/signalk/artemis/tdb/LogbookDbService.java
+++ src/main/java/nz/co/fortytwo/signalk/artemis/tdb/LogbookDbService.java
@@ -6,9 +6,11 @@ import org.influxdb.InfluxDBException;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Point;
 
+import java.util.Map;
 import java.util.NavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class LogbookDbService {
 
@@ -26,26 +28,65 @@ public class LogbookDbService {
         logbookInfluxDB.setDatabase("logbook");
     }
 
-    public void saveToLogbook(String event) {
+
+    public void saveToLogbook(String eventType) {
+        String StW = "null";
         // Get necessary information from vessels measurement
         String query = "select * from vessels group by skey, primary, uuid, sourceRef order by time desc limit 1";
         NavigableMap<String, Json> map = new ConcurrentSkipListMap<>();
         artemisInfluxDB.loadData(map, query);
-        map.forEach((k, v) -> {
-            System.out.println("key: " + k + "value: " + "\n" + v.at("value"));
-            if(k.contains("navigation.positionvalue:")) {
+
+        /*for(Map.Entry<String, Json> entry : map.entrySet()) {
+            System.out.println("key: " + entry.getKey() + "\nvalue: " + entry.getValue() + "\n" + entry.getValue().at("value"));
+            if(entry.getKey().equals("vessels.urn:mrn:signalk:uuid:71c9c917-2d2c-495a-919d-17ab02411f6c.navigation.speedThroughWater")) {
                 System.out.println("FOUND KEY");
+                StW = entry.getValue().at("value").toString();
             }
-        } );
-        String SoG = map.get("vessels.urn:mrn:signalk:uuid:71c9c917-2d2c-495a-919d-17ab02411f6c.navigation.speedOverGround:").toString();
+        }*/
+
+        /*map.forEach((k, v) -> {
+            System.out.println("key: " + k + "value: " + "\n" + v.at("value"));
+            if(k.equals("vessels.urn:mrn:signalk:uuid:71c9c917-2d2c-495a-919d-17ab02411f6c.navigation.speedThroughWater")) {
+                System.out.println("FOUND KEY");
+                //StW = v.at("value").at("value").toString();
+            }
+        } );*/
+
+        String SoGValue = getValue(map, "navigation.speedOverGround");
+        String StWValue = getValue(map, "navigation.speedThroughWater");
+        String[] posValues = getValue(map, "navigation.position").split(",");
         // create a measurement entry
         logbookInfluxDB.write(Point.measurement("event")
-            .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
-            .addField("eventType", event)
-            .addField("position", "getPos1")
-            .addField("SoG", "m/s")
-            .addField("StW", "m/s")
-            .build());
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("eventType", eventType)
+                .addField("posLat", posValues[0])
+                .addField("posLong", posValues[1])
+                .addField("SoG", SoGValue)
+                .addField("StW", StWValue)
+                .build());
+    }
+
+    private String getValue(NavigableMap<String, Json> map, String keyDescription) {
+        String retVal = "null";
+        String strKey = map.keySet().stream().filter(s -> s.endsWith(keyDescription)).collect(Collectors.toSet()).toString();
+        if(strKey.contains("[")) {
+            strKey = strKey.replace("[", "").trim();
+        }
+        if(strKey.contains("]")) {
+            strKey = strKey.replace("]", "").trim();
+        }
+
+        if (keyDescription == "navigation.position") {
+            // create [] with latitude, longitude & altitude
+            String latitude = map.get(strKey).at("value").at("latitude").toString();
+            String longitude = map.get(strKey).at("value").at("longitude").toString();
+            //String altitude = map.get(strKey).at("value").at("longitude").toString(); // currently missing in test set
+            retVal = latitude.concat(", ").concat(longitude);
+        } else {
+            retVal = map.get(strKey).at("value").toString();
+        }
+
+        return retVal.trim();
     }
 
     /**
