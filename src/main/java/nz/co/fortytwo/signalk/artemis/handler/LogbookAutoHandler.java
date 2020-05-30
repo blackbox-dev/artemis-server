0c3dd5dcf78dc16272d36f1f34c05c503c1e80b5
Jaromir <jaromircharles@hotmail.com>
2020-05-30 12:14:29 +0200

    removed unused code

--- src/main/java/nz/co/fortytwo/signalk/artemis/handler/LogbookAutoHandler.java
+++ src/main/java/nz/co/fortytwo/signalk/artemis/handler/LogbookAutoHandler.java
@@ -1,15 +1,9 @@
 package nz.co.fortytwo.signalk.artemis.handler;
 
-import mjson.Json;
 import nz.co.fortytwo.signalk.artemis.tdb.LogbookDbService;
-import nz.co.fortytwo.signalk.artemis.util.Util;
 import org.apache.activemq.artemis.api.core.Message;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.time.LocalDateTime;
-import java.util.NavigableMap;
-import java.util.concurrent.ConcurrentSkipListMap;
 
 import static nz.co.fortytwo.signalk.artemis.util.Config.AMQ_INFLUX_KEY;
 import static nz.co.fortytwo.signalk.artemis.util.SignalKConstants.*;
@@ -25,26 +19,16 @@ public class LogbookAutoHandler extends BaseHandler {
 			logger.debug("Initialising for : {} ", uuid);
 		try {
 			initSession(AMQ_INFLUX_KEY + " LIKE '%" + logbook + dot + auto + "%'");
-			// Start new thread
-			// Get last auto entry from DB
-			// lastAutoEntryTime = influx.getLastEntry();
+					//+ AMQ_INFLUX_KEY + " LIKE '" + vessels + "%'");
 			logbookInfluxDB = new LogbookDbService();
-			//logbookAutoThread.start();
 		} catch (Exception e) {
 			logger.error(e, e);
 		}
 	}
 
-
 	@Override
 	public void consume(Message message) {
-
-		String key = message.getStringProperty(AMQ_INFLUX_KEY);
-		Json node = Util.readBodyBuffer( message.toCore());
-
 		logbookInfluxDB.saveToLogbook("auto");
-
-		return;
 	}
 
 }
