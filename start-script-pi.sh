59d6dbb6707d540438812fdf58a7825d6706bed8
Kuba1993 <David.Kubatzki@htwg-konstanz.de>
2020-07-15 14:49:31 +0200

    fixed Docker and adjusted the start-scripts

--- start-script-pi.sh
+++ start-script-pi.sh
@@ -1,40 +1,45 @@
-sudo apt-get install wget
-sudo apt-get install git
-sudo apt-get install maven
+yes Yes | sudo apt-get install wget
+yes Yes | sudo apt-get install git
+yes Yes | sudo apt-get install maven
 
-sudo apt-get install \
+yes Yes | sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+yes Yes | curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 sudo apt update
 apt-cache policy docker-ce
-sudo apt install docker-ce
+yes Yes | sudo apt install docker-ce
 
 
-echo "deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ jessie main" | sudo tee /etc/apt/sources.list.d/hypriot.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 37BBEE3F7AD95B3F
-sudo apt-get update
-sudo apt-get install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-git clone https://github.com/blackbox-dev/artemis-server.git
-cd artemis-server
-git checkout dockerBuild
-sudo chmod 777 docker/signalk/signalk_entrypoint.sh
-mvn package -Dmaven.test.skip=true
+#install jdk
+yes Yes | sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt install openjdk-11-jdk
+sudo chmod 777 /etc/environment
+sudo echo "JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/" >> /etc/environment
+sudo . /etc/environment 
 
+#download the artemis-server
+git clone https://github.com/blackbox-dev/artemis-server.git
+cd /artemis-server
+#checkout correct branch for building docker
+sudo git checkout persistToInflux 
+sudo mvn clean install -Dmaven.test.skip=true
+sudo chmod 777 docker/signalk/signalk_entrypoint.sh
+
+#creates directory for telegraf config
+###config has to be added manually###
 sudo mkdir /etc/telegraf && sudo touch /etc/telegraf/telegraf.conf
-sudo docker run --rm telegraf telegraf config | sudo tee /etc/telegraf/telegraf.conf
-## replace with our config ##
 
 sudo docker build --tag blackbox:signalk -f docker/signalk/Dockerfile .
 sudo docker build --tag blackbox:influxdb -f docker/influxdb/Dockerfile .
 sudo docker pull telegraf
 
-sudo docker-compose -f docker-compose-pi.yml up
+sudo docker-compose up
