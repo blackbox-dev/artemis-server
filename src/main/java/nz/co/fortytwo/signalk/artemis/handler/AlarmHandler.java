7ac7f3f3423fae9db9a9518bf32f3d43472c0f21
GITBSB <bsbenny.bh@gmail.com>
2020-08-03 13:12:56 +0200

    fixed alarmhandler

--- src/main/java/nz/co/fortytwo/signalk/artemis/handler/AlarmHandler.java
+++ src/main/java/nz/co/fortytwo/signalk/artemis/handler/AlarmHandler.java
@@ -28,34 +28,34 @@ import nz.co.fortytwo.signalk.artemis.tdb.TDBService;
 import nz.co.fortytwo.signalk.artemis.util.Util;
 
 /*
-*
-* Copyright (C) 2012-2014 R T Huitema. All Rights Reserved.
-* Web: www.42.co.nz
-* Email: robert@42.co.nz
-* Author: R T Huitema
-*
-* This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-* WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-* 
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-*/
+ *
+ * Copyright (C) 2012-2014 R T Huitema. All Rights Reserved.
+ * Web: www.42.co.nz
+ * Email: robert@42.co.nz
+ * Author: R T Huitema
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 /**
  * Read key value pairs from Config.INTERNAL_KV and store in influxdb
- * 
+ *
  * @author robert
- * 
+ *
  */
 
 public class AlarmHandler extends BaseHandler {
@@ -135,6 +135,9 @@ public class AlarmHandler extends BaseHandler {
 		if (metaJson == null) {
 			metaJson = Json.object();
 		}
+		if(node.has("value")) {
+			node = node.at("value");
+		}
 		Util.setJson(metaJson, metaKey, node);
 		alarmMap.put(parentKey, metaJson);
 		if (logger.isDebugEnabled())
@@ -189,7 +192,6 @@ public class AlarmHandler extends BaseHandler {
 				}
 			}
 		}
-
 	}
 
 	private Json getNotification(String state, Json message, Json warnMethod) {
